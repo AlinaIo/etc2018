4cc89fca615bec527c69f31ac1716456bc9dc9a2
AlinaIo <alinaionescu85@gmail.com>
2018-02-19 01:19:30 +0200

    etc with v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/">
-          <h1>Expensify v2</h1>
+          <h1>Expensify v1</h1>
         </Link>
         <Link className="button" to="/create">Add Expense</Link>
       </div>
